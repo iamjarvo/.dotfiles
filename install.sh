73315fd2f578d8f3feb4d3ba45e3d3ef937e9641
Jearvon Dharrie <j.dharrie@gmail.com>
2014-06-14 16:38:08 -0400

    Use PWD instead of hard coded directory

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 link_file() {
-  ln -vs ~/.dotfiles/$1 ~/$1 || (rm -rf ~/$1 && ln -vs ~/.dotfiles/$1 ~/$1)
+  ln -vs $PWD/$1 ~/$1 || (rm -rf ~/$1 && ln -vs $PWD/$1 ~/$1)
 }
 
 link_file ".vimrc"
