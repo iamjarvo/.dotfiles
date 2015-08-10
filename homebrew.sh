2f0a9650b4566f2446ba5ebd6fcf05fd785b0a7b
Jearvon Dharrie <j.dharrie@gmail.com>
2015-08-10 15:10:37 -0400

    Add homebrew

---
+++ homebrew.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+which -s brew && brew update
+which -s brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+formulae=(
+  ag
+  caskroom/cask/brew-cask
+  chruby
+  emacs --with-coca
+  git
+  hub
+  jq
+  pick
+  ruby-install
+  tree
+  wget
+)
+
+casks=(
+  1password
+  alfred
+  dropbox
+  iterm2
+  skype
+  slack
+  slate
+)
+
+for formula in "${formulae[@]}"; do
+  brew install $formula || brew upgrade $formula
+done
+
+brew linkapps
+
+for cask in "${casks[@]}"; do
+  brew cask install $cask
+done
