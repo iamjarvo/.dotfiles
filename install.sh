327393c90a8a814428ba40f87826df5d8a3489b5
Jearvon Dharrie <j.dharrie@gmail.com>
2015-08-10 15:13:02 -0400

    Add git email and name

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ link_file ".slate"
 link_file ".gemrc"
 link_file ".tmux.conf"
 cp -r $PWD/.vim ~/.
+
+git config --global user.email "j.dharrie@gmail.com"
+git config --global user.name "Jearvon Dharrie"
