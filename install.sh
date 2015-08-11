a98bf079ab9fc11e314e37017b924f40c60698f1
Jearvon Dharrie <j.dharrie@gmail.com>
2015-08-10 23:30:54 -0400

    Add osx defaults

--- install.sh
+++ install.sh
@@ -12,3 +12,18 @@ cp -r $PWD/.vim ~/.
 
 git config --global user.email "j.dharrie@gmail.com"
 git config --global user.name "Jearvon Dharrie"
+
+# dock
+defaults write com.apple.dock orientation right
+defaults write com.apple.dock autohide 1
+defaults write com.apple.dock tilesize 16
+
+# tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+# enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# key repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 0
